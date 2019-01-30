ba9b58ff286611e2f69ddb5fe444536da792f511
Neeff <dpincheira2016@alu.uct.cl>
2019-01-30 11:03:23 -0300

    add:segundo commit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,4 +5,12 @@ class TodosController < ApplicationController
   def new
     @todos = Todo.new
   end
+  def create
+    @todos = Todo.new(todos_params)
+    @todos.save
+  end
+  private
+  def todos_params
+    params.require(:todo).permit(:description)
+  end
 end
